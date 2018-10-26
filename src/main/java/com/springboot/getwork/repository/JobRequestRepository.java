3b8e2a0a345d84b781b80fc1b4cb4a2bc20959de
Carlo Janea <janeacarlo@gmail.com>
2018-10-26 21:50:16 +0800

    fixed update for jobrequest

--- src/main/java/com/springboot/getwork/repository/JobRequestRepository.java
+++ src/main/java/com/springboot/getwork/repository/JobRequestRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.springboot.getwork.model.Company;
 import com.springboot.getwork.model.JobRequest;
+import com.springboot.getwork.model.JobRequest.JobStatus;
 
 import java.util.List;
 
@@ -16,6 +17,6 @@ public interface JobRequestRepository extends JpaRepository<JobRequest, Integer>
     List<JobRequest> findAllByCompanyId(Integer companyId);
 
     //@Query("SELECT j FROM JobRequest j WHERE j.status =(:findkey) OR j.jobRequestNumber like %:findkey% AND j.company_id = (:comp_id)")
-    @Query("SELECT j FROM JobRequest j LEFT JOIN j.company c WHERE c.id = (:comp_id) AND j.status = (:findkey) OR j.jobRequestNumber like %:findkey%")
-    List<JobRequest> findJobRequestsByJobRequestNumberOrStatus(@Param("findkey") String findkey, @Param("comp_id") Integer company_id);
+    @Query("SELECT j FROM JobRequest j LEFT JOIN j.company c WHERE c.id = (:comp_id) AND j.status = (:statusKey) OR j.jobRequestNumber like %:numberKey%")
+    List<JobRequest> findJobRequestsByJobRequestNumberOrStatus(@Param("numberKey") String numberKey, @Param("statusKey") JobStatus statusKey, @Param("comp_id") Integer company_id);
 }
