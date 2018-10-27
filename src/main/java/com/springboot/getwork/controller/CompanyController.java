4819815450430f572d0f4a958b22dbd3f45bed64
Carlo Janea <janeacarlo@gmail.com>
2018-10-27 21:47:36 +0800

    changed uri for company screen landing page

--- src/main/java/com/springboot/getwork/controller/CompanyController.java
+++ src/main/java/com/springboot/getwork/controller/CompanyController.java
@@ -16,7 +16,6 @@ import com.springboot.getwork.service.CompanyServiceImpl;
  *
  */
 @RestController
-@RequestMapping("/companies")
 @CrossOrigin(value = "http://localhost:4200/")
 public class CompanyController {
 
@@ -48,14 +47,14 @@ public class CompanyController {
         companyService.registerCompany(company);
         return "Saved company";
     }
-    //
+
     @GetMapping(path = "/getAll")
     public @ResponseBody List<Company> getAllCompanies() {
         // This returns a JSON or XML with the users
         return companyService.getAll();
     }
 
-    @GetMapping(path = "/getOne/{company_id}")
+    @GetMapping(path = "/{company_name}/{company_id}")
     public @ResponseBody Company getCompanyInfo(@PathVariable("company_id") Integer company_id) {
         // This returns a JSON or XML with the users
         return companyService.getCompanyInfo(company_id);
