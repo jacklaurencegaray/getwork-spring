5705da713bb843c458f7d33c1b9d3230f4cdbc04
thessaquijote <thessaangelie_quijote@yahoo.com>
2018-11-26 21:10:17 +0800

    Fixed JobRequestRepository errors

--- src/main/java/com/springboot/getwork/repository/JobRequestRepository.java
+++ src/main/java/com/springboot/getwork/repository/JobRequestRepository.java
@@ -1,13 +1,10 @@
 package com.springboot.getwork.repository;
 
-import javafx.print.PrinterJob;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
-import com.springboot.getwork.model.Company;
 import com.springboot.getwork.model.JobRequest;
 import com.springboot.getwork.model.JobRequest.JobStatus;
 
@@ -16,11 +13,7 @@ import java.util.List;
 @Repository
 public interface JobRequestRepository extends JpaRepository<JobRequest, Integer> {
 
-//    @Async // method is executed asynchronously, try2 lng if d mu.refresh ang page ani lol
     List<JobRequest> findAllByCompanyId(Integer companyId);
-
-//    to be continued
-    JobRequest findJobRequestByCompanyIdAndId(Integer companyId, Integer id);
 
     //@Query("SELECT j FROM JobRequest j WHERE j.status =(:findkey) OR j.jobRequestNumber like %:findkey% AND j.company_id = (:comp_id)")
     @Query("SELECT j FROM JobRequest j LEFT JOIN j.company c WHERE c.id = (:comp_id) AND j.status = (:statusKey) OR j.jobRequestNumber like %:numberKey%")
